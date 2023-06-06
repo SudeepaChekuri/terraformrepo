33ea988299a6e459aeab47a4d6f6264deb274323
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-06 22:26:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,11 +37,6 @@ variable "ec2_instance_type" {
   default     = "t2.micro"
 }
 
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
 
 resource "aws_s3_bucket" "terfbuc" {
   bucket = var.s3_bucket_name
