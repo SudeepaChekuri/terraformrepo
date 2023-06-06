29fcf666b3431f3e7d6b8118b4bfae38406d2641
SudeepaChekuri <98836806+SudeepaChekuri@users.noreply.github.com>
2023-06-06 20:58:06 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,29 +1,39 @@
-variable "aws_region" {
-  description = "AWS Region"
-  default     = "ap-south-1"
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+
+variable "s3_bucket_name" {
+  type        = string
+  description = "S3 Bucket Name"
+  default     = "terfbuc"
 }
 
 variable "vpc_cidr_block" {
+  type        = string
   description = "VPC CIDR block"
   default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr_block" {
+  type        = string
   description = "Subnet CIDR block"
   default     = "10.0.0.0/24"
 }
 
-variable "s3_bucket_name" {
-  description = "S3 Bucket"
-  default     = "terfbuc"
-}
-
 variable "ec2_ami" {
+  type        = string
   description = "EC2 AMI ID"
   default     = "ami-0607784b46cbe5816"
 }
 
 variable "ec2_instance_type" {
+  type        = string
   description = "EC2 Instance Type"
   default     = "t2.micro"
 }
